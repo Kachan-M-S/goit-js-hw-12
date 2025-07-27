7862b8ad1a2225a04b44746b0253a8651961fa4d
Kachan-M-S <misha.2002.kachan@gmail.com>
2025-07-27 15:57:24 +0300

    add project

---
+++ src/main.js
@@ -0,0 +1,136 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+  showLoadMoreButton,
+  hideLoadMoreButton,
+} from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let currentPage = 1;
+let currentQuery = '';
+let totalHits = 0;
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const query = input.value.trim();
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search term',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  currentQuery = query;
+  currentPage = 1;
+  clearGallery();
+  hideLoadMoreButton();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(currentQuery, currentPage);
+    totalHits = data.totalHits;
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+    } else {
+      createGallery(data.hits);
+      if (totalHits > currentPage * 15) {
+        showLoadMoreButton();
+      }
+    }
+  } catch (error) {
+    iziToast.error({
+      message: 'An error occurred. Please try again later.',
+      position: 'topRight',
+    });
+  } finally {
+    hideLoader();
+  }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  currentPage += 1;
+  showLoader();
+  hideLoadMoreButton();
+
+  try {
+    const data = await getImagesByQuery(currentQuery, currentPage);
+    createGallery(data.hits);
+
+    smoothScroll();
+
+    const totalPages = Math.ceil(totalHits / 15);
+    if (currentPage < totalPages) {
+      showLoadMoreButton();
+    } else {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+    }
+  } catch (error) {
+    iziToast.error({
+      message: 'An error occurred. Please try again later.',
+      position: 'topRight',
+    });
+  } finally {
+    hideLoader();
+  }
+});
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+const inputHolder = document.getElementById('typewriter');
+const words = [
+  'Search images...',
+  'Find your favorite...',
+  'Try "nature" or "sky"',
+];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentWord = words[wordIndex];
+  const visibleText = currentWord.slice(0, charIndex);
+  inputHolder.setAttribute('placeholder', visibleText);
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(typeEffect, 50);
+  } else {
+    isDeleting = !isDeleting;
+    if (!isDeleting) {
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+    setTimeout(typeEffect, 1000);
+  }
+}
+
+typeEffect();
